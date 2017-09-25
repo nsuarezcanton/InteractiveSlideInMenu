e11ae5744b8a517e318002788e70e5c52875e94f
Pablo Suarez-Canton <pablo@zeez.io>
2017-09-25 16:15:56 +0200

    Removed dismissal method from `MenuViewController`.

--- InteractiveSlideInMenu/MenuViewController.swift
+++ InteractiveSlideInMenu/MenuViewController.swift
@@ -34,48 +34,10 @@ class MenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		self.view.backgroundColor = UIColor.green
-		
-		let panRight = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanRight))
-		self.view.addGestureRecognizer(panRight)
     }
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
-	}
-
-	
-	// MARK: View setup
-	
-	func handlePanRight(_ gesture: UIPanGestureRecognizer) {
-		guard let view: UIView = gesture.view else {
-			return
-		}
-		
-		let translate = gesture.translation(in: view)
-		let percent = -translate.x / view.bounds.size.width
-		
-		switch gesture.state {
-			case UIGestureRecognizerState.began:
-				interactionController = UIPercentDrivenInteractiveTransition()
-				customTransitionDelegate.interactionController = interactionController
-				
-				dismiss(animated: true)
-			case UIGestureRecognizerState.changed:
-				interactionController?.update(percent)
-			case UIGestureRecognizerState.cancelled:
-				fallthrough
-			case UIGestureRecognizerState.ended:
-				if (percent > 0.60) {
-					interactionController?.finish()
-				} else {
-					interactionController?.update(0.0)
-					interactionController?.cancel()
-				}
-				
-				interactionController = nil
-			default:
-				break
-		}
 	}
 }
